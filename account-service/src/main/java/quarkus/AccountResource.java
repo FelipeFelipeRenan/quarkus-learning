16b643405d92c0ada982189db97845551ba2fe41
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-11-15 20:21:42 -0300

    [DEV] Adding mock data to account service

--- account-service/src/main/java/quarkus/AccountResource.java
+++ account-service/src/main/java/quarkus/AccountResource.java
@@ -1,8 +1,35 @@
 package quarkus;
 
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/accounts")
 public class AccountResource {
+
+Set<Account> accounts = new HashSet<>(); 
+ 
+@PostConstruct 
+public void setup() { 
+ 
+    accounts.add(new Account(123456789L, 987654321L, "George Baird", new 
+
+    BigDecimal("354.23")));
+    accounts.add(new Account(121212121L, 888777666L, "Mary Taylor", new 
+    BigDecimal("560.03")));
+    accounts.add(new Account(545454545L, 222444999L, "Diana Rigg", new 
+    BigDecimal("422.00")));
+ }
     
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<Account> allAccounts(){
+        return accounts;
+    }
 }
