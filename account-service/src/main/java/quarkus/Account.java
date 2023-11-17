8cf0d3ef8a7c1d947d33818c493060a3d69ff36d
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-11-17 18:43:23 -0300

    [DEV] Adding tests

--- account-service/src/main/java/quarkus/Account.java
+++ account-service/src/main/java/quarkus/Account.java
@@ -1,6 +1,7 @@
 package quarkus;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Account {
     public Long accountNumber;
@@ -9,7 +10,7 @@ public class Account {
     public BigDecimal balance;
     public AccountStatus accountStatus = AccountStatus.OPEN;
 
-    public Account(){
+    public Account() {
     }
 
     public Account(Long accountNumber, Long customerNumber, String customerName, BigDecimal balance) {
@@ -23,20 +24,24 @@ public class Account {
         accountStatus = AccountStatus.OVERDRAWN;
     }
 
-    public void removeOverdrawnStatus(){
+    public void removeOverdrawnStatus() {
         accountStatus = AccountStatus.OPEN;
     }
 
-    public void close(){
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void close() {
         accountStatus = AccountStatus.CLOSED;
         balance = BigDecimal.valueOf(0);
     }
 
-    public void withdrawnFunds(BigDecimal amount){
+    public void withdrawnFunds(BigDecimal amount) {
         balance = balance.subtract(amount);
     }
 
-    public void addfunds(BigDecimal amount){
+    public void addFunds(BigDecimal amount) {
         balance = balance.add(amount);
     }
 
@@ -56,6 +61,20 @@ public class Account {
         return accountStatus;
     }
 
-    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Account account = (Account) o;
+        return accountNumber.equals(account.accountNumber) &&
+                customerNumber.equals(account.customerNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber, customerNumber);
+    }
 
 }
