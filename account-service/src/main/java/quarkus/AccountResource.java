7b90e9c3cd4737b594c91622083e50e3d958ba80
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-11-17 15:53:29 -0300

    [DEV] Adding getAccount route

--- account-service/src/main/java/quarkus/AccountResource.java
+++ account-service/src/main/java/quarkus/AccountResource.java
@@ -2,34 +2,47 @@ package quarkus;
 
 import java.math.BigDecimal;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/accounts")
 public class AccountResource {
 
-Set<Account> accounts = new HashSet<>(); 
- 
-@PostConstruct 
-public void setup() { 
- 
-    accounts.add(new Account(123456789L, 987654321L, "George Baird", new 
+    Set<Account> accounts = new HashSet<>();
 
-    BigDecimal("354.23")));
-    accounts.add(new Account(121212121L, 888777666L, "Mary Taylor", new 
-    BigDecimal("560.03")));
-    accounts.add(new Account(545454545L, 222444999L, "Diana Rigg", new 
-    BigDecimal("422.00")));
- }
-    
+    @PostConstruct
+    public void setup() {
+
+        accounts.add(new Account(123456789L, 987654321L, "George Baird", new
+
+        BigDecimal("354.23")));
+        accounts.add(new Account(121212121L, 888777666L, "Mary Taylor", new BigDecimal("560.03")));
+        accounts.add(new Account(545454545L, 222444999L, "Diana Rigg", new BigDecimal("422.00")));
+    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Set<Account> allAccounts(){
+    public Set<Account> allAccounts() {
         return accounts;
+    }
+
+    @GET
+    @Path("/{accountNumber}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Account geAccount(@PathParam("accountNumber") Long accountNumber) {
+        Optional<Account> response = accounts.stream().filter(acct -> acct.getAccountNumber().equals(accountNumber))
+                .findFirst();
+
+        return response
+                .orElseThrow(() -> new NotFoundException("Account with id of " + accountNumber + "does not exist"));
+
     }
 }
