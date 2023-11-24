203f5914f0d7993eb185c27d2543006c1077711a
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-11-24 17:59:39 -0300

    [DEV] Adding in memory reservation

---
+++ rental/car-rental/src/main/java/org/acme/repository/InMemoryReservationRepository.java
@@ -0,0 +1,25 @@
+package org.acme.repository;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+import org.acme.model.Reservation;
+
+public class InMemoryReservationRepository implements ReservationRepository{
+
+    private final AtomicLong ids  = new AtomicLong(0);
+    private final List<Reservation> store = new CopyOnWriteArrayList<>();
+    
+    @Override
+    public List<Reservation> findAll(){
+        return Collections.unmodifiableList(store);
+    }
+
+    @Override
+    public Reservation save(Reservation reservation){
+        reservation.setId(ids.incrementAndGet());
+        store.add(reservation);
+        return reservation;
+    }
+}
