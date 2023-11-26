3f83d3d8c22fb19054e8c9fee35d0d164c795300
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-11-26 17:48:42 -0300

    [DEV] Adding mock database and car model to repository service

---
+++ rental/repository-service/src/main/java/org/acme/database/CarInventory.java
@@ -0,0 +1,43 @@
+package org.acme.database;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.acme.model.Car;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class CarInventory {
+    private List<Car> cars;
+
+    public static final AtomicLong ids = new AtomicLong(0);
+
+    @PostConstruct
+    void initialize() {
+        cars = new CopyOnWriteArrayList<>();
+        initialData();
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    private void initialData() {
+        Car mazda = new Car();
+        mazda.setId(ids.incrementAndGet());
+        mazda.setManufacturer("Mazda");
+        mazda.setModel("6");
+        mazda.setLicensePlateNumber("ABC123");
+        cars.add(mazda);
+
+        Car ford = new Car();
+        ford.setId(ids.incrementAndGet());
+        ford.setManufacturer("Ford");
+        ford.setModel("Mustang");
+        ford.setLicensePlateNumber("XYZ987");
+        cars.add(ford);
+    }
+}
